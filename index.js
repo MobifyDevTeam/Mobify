231c944f2371d1c13430b22465f7ee0a02b63e35
G10sr <giosr295@gmail.com>
2025-03-18 20:33:51 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const fs = require('fs').promises;  
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use('/img', express.static('img'));
+app.use('/css', express.static('css'));
+
+
+app.get('/', async (req, res) => {
+  try {
+    const content = await fs.readFile(( './html/home.html'), 'utf-8');
+    res.send(content);
+  } catch (err) {
+    res.status(500).send('Error en Home.');
+  }
+});
+
+
+app.get('/introduccion', async (req, res) => {
+  try {
+    const content = await fs.readFile(('./html/introduccion.html'), 'utf-8');
+    res.send(content);
+  } catch (err) {
+    res.status(500).send('Error en Introduccion.');
+  }
+});
+
+app.get('/misionvision', async (req, res) => {
+    try {
+      const content = await fs.readFile(('./html/misionvision.html'), 'utf-8');
+      res.send(content);
+    } catch (err) {
+      res.status(500).send('Error en Introduccion.');
+    }
+});
+  
+app.get('/introduccion', async (req, res) => {
+    try {
+      const content = await fs.readFile(('./html/introduccion.html'), 'utf-8');
+      res.send(content);
+    } catch (err) {
+      res.status(500).send('Error en Introduccion.');
+    }
+});
+  
+app.get('/objetivos', async (req, res) => {
+    try {
+      const content = await fs.readFile(('./html/objetivos.html'), 'utf-8');
+      res.send(content);
+    } catch (err) {
+      res.status(500).send('Error en Introduccion.');
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
